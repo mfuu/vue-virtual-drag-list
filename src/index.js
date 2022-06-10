f48ccd53a4009d059a6fd0b228fe1f37cacc0a02
mfuu <446310957@qq.com>
2022-06-10 14:26:27 +0800

    fix bug
    Fix bug of offset change when scrolling down or right

--- src/index.js
+++ src/index.js
@@ -226,6 +226,13 @@ const VirtualDragList = Vue.component('virtual-drag-list', {
             this._updateUniqueKeys()
             this.virtual.updateUniqueKeys(this.uniqueKeys)
           }
+          // recalculate the range once when scrolling down
+          if (this.sortable.rangeIsChanged && this.virtual.direction === 'BEHIND') {
+            if (this.range.start > 0) {
+              this.range.start -= 1
+              this.range.end -= 1
+            }
+          }
           this._clearDragState()
         }
       )
